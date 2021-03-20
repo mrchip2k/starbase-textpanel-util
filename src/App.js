26182c282e43d21dad667f94df6adca363a949e4
mrchip2k <alemetzen3@gmail.com>
2021-03-20 18:39:35 +0100

    Import React Router, Soon image, move App.css

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
-import './App.css';
+import './css/App.css';
+
+import {
+	BrowserRouter as Router,
+	Switch,
+	Route
+} from "react-router-dom";
 
 function App() {
 	return (
 		<div>
-			App.js is empty.
+			<Router>
+				<Switch>
+					<Route exact path="/"> Homepage Content </Route>
+					<Route path="/about"> About Page Content </Route>
+				</Switch>
+			</Router>
 		</div>
 	);
 }
