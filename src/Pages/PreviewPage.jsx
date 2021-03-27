518c477877aa202143b480563b367b9d0c2863a8
mrchip2k <alemetzen3@gmail.com>
2021-03-27 14:57:08 +0100

    Change title and layout in Editor page

--- src/Pages/PreviewPage.jsx
+++ src/Pages/PreviewPage.jsx
@@ -7,16 +7,24 @@ function PreviewPage(props) {
 	return (
 		<Container addClass="page-content">
 			<VersionBadge/>
-			<h1 className="centered">Preview Editor</h1>
+			<h1 className="centered">Editor</h1>
 
-			<img
-				src="/assets/soon.png"
-				alt="Soon" 
-				className="centered"
-			/>
+			<div className="row">
+				<div className="ten columns">
+					<h4>Oh no! This feature isn't available yet.</h4>
+					<p>The Editor will let you enter a YOLOL string and preview what it will look like on a textpanel in the game.</p>
+				</div>
+				
+				<div className="two columns">
+					<img
+						src="/assets/soon.png"
+						alt="Soon" 
+						className="centered"
+					/>
+				</div>
+			</div>
 
-			<h4>Oh no! This feature isn't available yet.</h4>
-			<p>The Editor will let you enter a YOLOL string and preview what it will look like on a textpanel in the game.</p>
+
 			<h4>By the way, here's another planned feature:</h4>
 			<p>
 				Rightclick to add characters to a textbox, then copy all of them at once. <span className="highlight">Includes keyboard shortcuts!</span> This will be the default mode for mobile users.
