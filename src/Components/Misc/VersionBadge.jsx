3d2bf18b85ea82b6a183f33bf37210fd7d5deb78
mrchip2k <alemetzen3@gmail.com>
2021-03-30 20:19:47 +0200

    Add _redirects file, bump version number

--- src/Components/Misc/VersionBadge.jsx
+++ src/Components/Misc/VersionBadge.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 function VersionBadge(props) {
 	return (
 		<div id="version-badge" className="telegrama">
-			<div> ⎯ yolol.lol ⎯ v1.0.0 ⎯ </div>
+			<div> ⎯ yolol.lol ⎯ v1.0.1 ⎯ </div>
 		</div>
 	);
 }
