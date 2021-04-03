89493319280508df678f1308399dc478ea31fe0b
mrchip2k <alemetzen3@gmail.com>
2021-04-03 21:48:11 +0200

    Remember the last displayed hint across page loads
    Also automatically shows the next time at every page load

--- src/Components/CharacterList/Hints.jsx
+++ src/Components/CharacterList/Hints.jsx
@@ -7,28 +7,41 @@ class Hints extends React.Component {
 	constructor (props) {
 		super(props);
 		this.state = {
-			hintIndex : this.readIndexFromStorage(),
+			hintIndex : 420, //this value should always be overwritten
 		};
 		this.hintsLength = hintsDatabase.length;
 		this.nextHint = this.nextHint.bind(this);
 	}
-
-	readIndexFromStorage() {
-		let read = parseInt( localStorage.getItem("lastHintID") );
-		// If it's not a valid number, reset to 0, otherwise use what was found in storage.
-		let result = ( typeof(read)==="number" && !isNaN(read) ) ? read : 0 ; 
-		return result;
+	
+	componentDidMount(){
+		this.initIndex();
 	}
 
-	nextHint(){
-		let result = this.state.hintIndex+1>=this.hintsLength ? 0 : this.state.hintIndex+1;
-		localStorage.setItem("lastHintID", result);
-		this.setState({ hintIndex: result });
+	setIndex(index) {
+		localStorage.setItem("lastHintID", index);
+		this.setState({ hintIndex: index });
+	}
+	nextHint(attemptedIndex){
+		let result = attemptedIndex+1 >= this.hintsLength ? 0 : attemptedIndex+1;
+		this.setIndex(result);
+	}
+
+	initIndex() {
+		let read = parseInt( localStorage.getItem("lastHintID") );
+		
+		if ( typeof(read)==="number" && !isNaN(read) ) {
+		//A valid number was found in storage. Use that.
+			//Also increment it, so each page load shows a different hint.
+			this.nextHint( read )
+		} else {
+		//Not a valid number. Probably the first page load. Set to 0.
+			this.setIndex( 0 )
+		}
 	}
 
 	render() {
 		return (
-			<div className="hints-container noselect" onClick={ this.nextHint }>
+			<div className="hints-container noselect" onClick={ ()=> {this.nextHint(this.state.hintIndex)} }>
 
 				<p> { hintsDatabase[this.state.hintIndex] } </p>
 
