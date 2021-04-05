4cb14d27c347840300e3c06a1ef6f02a24b17655
mrchip2k <alemetzen3@gmail.com>
2021-04-05 08:34:45 +0200

    Bump version number

--- src/Components/Misc/VersionBadge.jsx
+++ src/Components/Misc/VersionBadge.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 function VersionBadge(props) {
 	return (
 		<div id="version-badge" className="telegrama">
-			<div> ⎯ yolol.lol ⎯ v1.1.0 ⎯ </div>
+			<div> ⎯ yolol.lol ⎯ v1.1.1 ⎯ </div>
 		</div>
 	);
 }
