da8a41782085471c16b9ff963608b6694a96c3ca
mrchip2k <alemetzen3@gmail.com>
2022-03-25 14:53:03 +0100

    v1.1.3 - update for new domain name

--- src/hintsDatabase.js
+++ src/hintsDatabase.js
@@ -1,10 +1,10 @@
 let hintsDatabase =
 [
-"yolol.lol: Find and copy-paste special characters for Starbase Text Panels!",
+"starbase-charmap: Find and copy-paste special characters for Starbase Text Panels!",
 "This website uses the same font as the game: characters will look the same.",
 "Ingame text is tiny! Some of these characters are particularly thin and hard to read in most situations. Remember to test how things look directly in the game.",
 "Some characters are not monospace.",
-"Please report any issues or missing characters! Visit the About page for more information.",
+"Please report any issues or missing characters!",
 ];
 
 export default hintsDatabase;
